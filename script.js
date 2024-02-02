9536c65ac7d0d5f77dde735b34d3712746324e9f
Florian <58337160+OmengU@users.noreply.github.com>
2024-02-02 15:11:07 +0100

    last changes

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
+    isInViewport = (element) => {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+    
     const scrollShift = () => {
         document.body.style.setProperty('--scroll', ((window.scrollY / (document.body.offsetHeight - window.innerHeight) * 100).toString() + "%"))
     }
@@ -20,10 +30,49 @@ document.addEventListener('DOMContentLoaded', (event) => {
             return;
         }
 
-        menu.innerHTML = `<ul id="menuList">
-        ${Links.map(link => `<li class="menuListElement"><a href="#${link.toLocaleLowerCase()}">${link}</a></li>`).join('')}
-        </ul>`;
+        setTimeout(function() {
+            menu.innerHTML = `<ul id="menuList">
+            ${Links.map(link => `<li class="menuListElement"><a href="#${link.toLocaleLowerCase()}">${link}</a></li>`).join('')}
+            </ul>`;
 
+            const links = document.querySelectorAll('.menuListElement a');
+
+            links.forEach(link => {
+                const id = link.getAttribute('href').split("#")[1];
+                const heading = document.getElementById(id);
+                if(isInViewport(heading)){
+                    link.parentElement.style.background = "white";
+                    link.style.color = "black";
+                }
+                link.addEventListener('click', function() {
+                    links.forEach(l => {
+                        l.parentElement.style.background = "black";
+                        l.style.color = "white";
+                    })
+
+                    link.parentElement.style.background = "white";
+                    link.style.color = "black";
+                });
+            });
+            window.addEventListener('scroll', function() {
+                links.forEach(link => {
+    
+                    link.parentElement.style.background = "black";
+                    link.style.color = "white";
+    
+                    const id = link.getAttribute('href').split("#")[1];
+                    const heading = document.getElementById(id);
+                    if (isInViewport(heading)) {
+                        link.parentElement.style.background = "white";
+                        link.style.color = "black";
+                    }
+                });
+            });
+
+        }, 160);
+
+        
+        
     }
 
     const revertMenuExpand = () => {
@@ -34,15 +83,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
         menuButton.style.display = "inline";
     }
 
-    menu.addEventListener("mouseover", menuExpand);
-    menu.addEventListener("mouseout", function (event) {
-        if (menu.contains(event.relatedTarget)) {
-            return;
-        }
-        revertMenuExpand();
-    });
-
-
+    if (window.matchMedia("only screen and (max-width: 600px)").matches) {
+        document.addEventListener('touchstart', function(event) {
+            document.addEventListener('touchstart', function(event) {
+                var isTouchInsideMenu = menu.contains(event.target);
+                var isMenuExpanded = menu.classList.contains("menuExpanded");
+            
+                if (!isTouchInsideMenu && isMenuExpanded) {
+                    revertMenuExpand();
+                }
+                if(isTouchInsideMenu && !isMenuExpanded){
+                    menuExpand();
+                }
+            });
+        });
+    } else {
+        menu.addEventListener("mouseover", menuExpand);
+        menu.addEventListener("mouseout", function (event) {
+            if (menu.contains(event.relatedTarget)) {
+                return;
+            }
+            revertMenuExpand();
+        });
+    }
 });
 
 const Links = ["Welcome", "About", "Skills", "Projects"];
